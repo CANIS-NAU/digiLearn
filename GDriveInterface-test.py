adbf20d37918d8b1510d78e64bd00b7590c6f656
SMKastrul <SMKastrul@gmail.com>
2021-03-14 13:43:50 -0700

    Moved rest files into server

--- GDriveInterface-test.py
+++ GDriveInterface-test.py
@@ -1,6 +1,9 @@
 from com.DigiLearn.DigitalBackpack.REST.Interpreters import GDriveInterpreter
+from com.DigiLearn.DigitalBackpack.Server.Django import AuthenticationManager as am
 
-cred = 'credentials.json'
+
+
+cred = 'webClientSecret.json'
 fileid = '1ZBc6zCt0uPDl2YVC16E0aLWik1I5PzBWxgzO_XdhMDw'
 
 meta = GDriveInterpreter.get_file_list(cred, "my-drive")
