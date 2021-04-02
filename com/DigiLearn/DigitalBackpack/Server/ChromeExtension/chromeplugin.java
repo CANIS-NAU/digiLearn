f90eddb99d5775b241a27a6cd7afb5ed98f8b314
graceshirey <39310649+graceshirey@users.noreply.github.com>
2021-04-02 15:28:47 -0700

    Add files via upload

---
+++ com/DigiLearn/DigitalBackpack/Server/ChromeExtension/chromeplugin.java
@@ -0,0 +1,35 @@
+package com.google.api.client.googleapis.auth.oauth2;
+
+import com.google.api.client.auth.openidconnect.IdToken;
+import com.google.api.client.auth.openidconnect.IdTokenVerifier;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.util.Beta;
+import com.google.api.client.util.Clock;
+import com.google.api.client.util.Preconditions;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.security.PublicKey;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(transport, jsonFactory)
+    .setAudience(Collections.singletonList(CLIENT_ID))
+    .build();
+
+GoogleIdToken idToken = verifier.verify(idTokenString);
+if (idToken != null) {
+  Payload payload = idToken.getPayload();
+
+  // Print user identifier
+  String userId = payload.getSubject();
+  System.out.println("User ID: " + userId);
+
+  // Get profile information from payload
+  String email = payload.getEmail();
+  boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
+
+} else {
+  System.out.println("Invalid ID token.");
+}
