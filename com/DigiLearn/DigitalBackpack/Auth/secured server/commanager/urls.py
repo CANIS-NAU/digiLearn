2f423b96fc87f70716664f748e85636ecd4ac30b
Caitlin Abuel <49536579+CodeBerrie@users.noreply.github.com>
2021-04-12 11:16:40 -0700

    update commanager files

--- com/DigiLearn/DigitalBackpack/Auth/secured server/commanager/urls.py
+++ com/DigiLearn/DigitalBackpack/Auth/secured server/commanager/urls.py
@@ -8,18 +8,31 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login, name='login'),
     path('accounts/', include('allauth.urls')),
-    path('', TemplateView.as_view(template_name="templates/index.html")),
+    url('auth/', views.MobileAuth), 
+    url('authWeb/', views.WebAuth), 
+    url('file/upload', views.FileUpload),
 
-    # example: "www.digipackweb.com/user/<gmail>"
-    re_path(r'^user/(?P<user>[\w.@+-]+)/$', views.InitDrive),
+    #path('', TemplateView.as_view(template_name="templates/index.html")),
+
+    # example: "www.digipackweb.com/drive/<gmail>"
+    re_path(r'^drive/(?P<user>[\w.@+-]+)/$', views.InitDrive),
+
+    # example: "www.digipackweb.com/gclass/<gmail>"
+    re_path(r'^gclass/(?P<user>[\w.@+-]+)/$', views.InitGClass),
 
     # example: "www.digipackweb.com/sd/<gmail>/<fileid>"
     re_path(r'^sd/(?P<user>[\w.@+-]+)/(?P<fileid>[\w.@+-]+)$', views.DriveServerDownload),
 
-    # example: "www.digipackweb.com/cd/<gmail>/<filename>"
-    #re_path(r'^cd/(?P<userid>[\w.@+-]+)/(?P<filename>[\w.@+-]+)$', views.DriveClientDownload),
     #path('goat.jpeg', views.DriveClientDownload),
     path('download/<str:filename>/', views.DriveClientDownload),
 
-    url('auth/', views.MobileAuth), #possibly remove the trailing "/"
+    # example: "www.digipackweb.com/pwa/<gmail>/<fileid>/<filename>"
+    path('pwa/<str:user>/<str:fileid>/<str:filename>/', views.DrivePWADownload),
 ]
+
+'''
+    url(r'^sw.js', cache_control(max_age=2592000)(TemplateView.as_view(
+    template_name="sw.js",
+    content_type='application/javascript',
+    )), name='sw.js'),
+'''
--- com/DigiLearn/DigitalBackpack/Auth/secured server/commanager/views.py
+++ com/DigiLearn/DigitalBackpack/Auth/secured server/commanager/views.py
@@ -1,20 +1,23 @@
-from django.shortcuts import render
+from django.views.decorators.csrf import ensure_csrf_cookie
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.csrf import ensure_csrf_cookie
-from django.http import HttpResponse
-from django.http import HttpRequest
-from django.http import JsonResponse
-from rest_framework.decorators import api_view
 from rest_framework.decorators import parser_classes
-from rest_framework.parsers import JSONParser
-from Auth import AuthenticationManager #TODO: add am to folder and change pathing on this import?
-import json
 from REST.Interpreters import GDriveInterpreter
+from REST.Interpreters import GClassInterpreter
+from rest_framework.decorators import api_view
+from rest_framework.parsers import JSONParser
+from django.http import HttpResponseRedirect
 from digipackDB import DatabaseManager
-import pathlib
+from Auth import AuthenticationManager 
 from django.http import FileResponse
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.http import HttpRequest
+from django.shortcuts import render
+from .forms import UploadFileForm
 import mimetypes
+import pathlib
+import json
 import os
 
 #@login_required(login_url='/login')
@@ -38,69 +41,48 @@ def MobileAuth(request):
     credentials = AM.redeemAuthCode(auth_json["googleAccessToken"])
 
     return JsonResponse({'Result' : 'ACK'})
+    
+    
+@api_view(['POST'])
+@parser_classes([JSONParser])
+@ensure_csrf_cookie
+def WebAuth(request):
+    #pass webclientsecret to auth manager
+    AM = AuthenticationManager.AuthenticationManager('webClientSecret.json')
+
+    #load the json data
+    auth_json = request.data
+
+    #pass access token to the auth manager
+    credentials = AM.redeemAuthCodeWeb(auth_json["googleAccessToken"])
+
+    return JsonResponse({'Result' : 'ACK'})    
+    
 
 #TODO: add a restriction that only allows a user to access their own url
 @api_view(['GET'])
 def InitDrive(request, user):
-    #pass webclientsecret to auth manager
-    #AM = AuthenticationManager.AuthenticationManager('webClientSecret.json')
-
-    #retrieve database creds
-    #credentials = AM.getCredentialsFromDatabase(user)
 
     secretFile = open("webClientSecret.json", "r")
     secretText = secretFile.read()
 
-    #print("webClientSecret" + secretText)
-
     # get file list // next try with drive list
     drivelist = GDriveInterpreter.get_file_list('webClientSecret.json', user, 'my-drive')
 
-    # extract the file names & file ids
-    # begin creating JSON object to pass to client
-    filedata = "[ "
+    filedata = []
 
-    #iterate through each dictionary
     for d in drivelist:
-        keylist = d.keys()
         if d.get("name") != None:
 
-            filedata =  filedata + "{"
-
-            # add the 'filename' as key
-            filedata =  filedata + "\"fileName\""
-
-            # separate key/value pairs with a colon
-            filedata = filedata + ":"
-
-            # add the file name as the value
-            filedata = filedata + "\"" + str(d.get("name"))  + "\""
-
-            # add comma
-            filedata = filedata + ", "
-
-            # add 'fileid' as key
-            filedata =  filedata + "\"fileid\""
-
-            # separate key/value pairs with a colon
-            filedata = filedata + ":"
-
-            # add the file id as the value
-            filedata = filedata + "\"" + str(d.get("driveID")) + "\""
-
-            filedata =  filedata + "}"
-
-            # add comma after except for last element
-            if d != drivelist[-1]:
-                filedata = filedata + ", "
-
-    # ending brace for JSON object
-    filedata = filedata + " ]"
+            val = {
+                "fileName": d["name"],
+                "fileid": d["driveID"]
+            }
+            filedata.append(val)
 
     # spit the file name list back
     return JsonResponse({"Files" : filedata})
 
-#TODO: make the url for this one, probably needs to pass in user and fileid
 #TODO: add restriction on this one too
 @api_view(['GET'])
 def DriveServerDownload(request, user, fileid):
@@ -111,10 +93,11 @@ def DriveServerDownload(request, user, fileid):
     #get credentials
     credentials = AM.getCredentialsFromDatabase(user)
 
+    #get file metadata and
     filemeta = GDriveInterpreter.get_file_metadata(credentials, user, fileid) 
     GDriveInterpreter.get_file('webClientSecret.json', user, filemeta)
     
-    print (filemeta)
+    #print (filemeta)
 
     return JsonResponse({'File Metadata' : filemeta})
 
@@ -129,3 +112,44 @@ def DriveClientDownload(request, filename):
     response['Content-Disposition'] = "attachment ; filename=%s" % filename
 
     return response
+
+@api_view(['GET'])
+def DrivePWADownload(request, user, fileid, filename):
+    #pass webclientsecret to auth manager
+    AM = AuthenticationManager.AuthenticationManager('webClientSecret.json')
+
+    #get credentials
+    credentials = AM.getCredentialsFromDatabase(user)
+
+    #get file metadata and
+    filemeta = GDriveInterpreter.get_file_metadata(credentials, user, fileid) 
+    GDriveInterpreter.get_file('webClientSecret.json', user, filemeta)
+
+    #use mimetype to determine the file type
+    file_path = './DriveStorage/' + filename
+    file_to_download = open(file_path, 'rb')
+    content_type = mimetypes.guess_type(filename)[0]
+    response = FileResponse(file_to_download, content_type=content_type)
+    response['Content-Disposition'] = "attachment ; filename=%s" % filename
+
+    return response
+
+@api_view(['POST'])
+def FileUpload(request):
+
+    path = './media/' + request.FILES[fileName]
+    print('This is the path: ' + path)
+    with open(path, 'wb+') as destination:
+        for chunk in request.FILES['uploaded_file'].chunks():
+            destination.write(chunk)
+    return HttpResponse('the file is saved')
+
+#TODO: add a restriction that only allows a user to access their own url
+@api_view(['GET'])
+def InitGClass(request, user):
+
+    # get class info
+    courses = GClassInterpreter.get_course_list('webClientSecret.json', user)
+
+    # spit the file name list back
+    return JsonResponse({"Courses" : courses})
