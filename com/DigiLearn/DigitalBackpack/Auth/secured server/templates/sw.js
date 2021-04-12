c5f4a714a889f830ce0c2e3b34192488340a5524
Caitlin Abuel <49536579+CodeBerrie@users.noreply.github.com>
2021-04-12 11:23:10 -0700

    update index and service worker

--- com/DigiLearn/DigitalBackpack/Auth/secured server/templates/sw.js
+++ com/DigiLearn/DigitalBackpack/Auth/secured server/templates/sw.js
@@ -10,6 +10,7 @@ self.addEventListener('install', function(event) {
   );
 });
 
+// this function caches the index page of the webapp
 self.addEventListener('fetch', function(event) {
   var requestUrl = new URL(event.request.url);
     if (requestUrl.origin === location.origin) {
@@ -18,12 +19,27 @@ self.addEventListener('fetch', function(event) {
         return;
       }
     }
+
     event.respondWith(
+      
+      // caches the main page
       caches.match(event.request).then(function(response) {
         return response || fetch(event.request);
+      }),
+
+      // dynamically caches 
+      caches.open('mysite-dynamic').then(function(cache) {
+        return fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
       })
+
     );
+
+
 });
+
 
 
 
