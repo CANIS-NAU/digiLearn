11b63fd5d080f20d759a09d0e42a91cb9e7d28e0
israel <ilb8@nau.edu>
2021-04-12 17:05:41 -0700

    GSearchInterpreter.py should be functional now.

--- com/DigiLearn/DigitalBackpack/Server/secured server/REST/Interpreters/GSearchInterpreter.py
+++ com/DigiLearn/DigitalBackpack/Server/secured server/REST/Interpreters/GSearchInterpreter.py
@@ -3,16 +3,19 @@ from REST.JSONBuilders import DigiJsonBuilder
 
 URL = "https://www.googleapis.com/customsearch/v1?"
 ENGINE_ID = "276d40ae1923ea29f"
-API_KEY = "AIzaSyDQ0aKnA5FC2JvcQCtIsl1XwjUsUNAik58"
+API_KEY = "AIzaSyCOAsr-SldNjop-SAQxb9HoY1E0KrIYDaE"
 PARAMS = {}
 
 
 def make_request(query):
-    resultsjson = {}
-    url = URL + "key=" + API_KEY + "&cx=" + ENGINE_ID + "&q=" + query
-
-    r = requests.get(url, PARAMS)
+    resultsarr = []
+    # build query url
+    u = URL + "key=" + API_KEY + "&cx=" + ENGINE_ID + "&q=" + query
+    # make request
+    r = requests.get(u, PARAMS)
+    # pull json from request
     res = r.json()
+    # pull "items" from results
     results = res["items"] if "items" in res else None
 
     if results is not None:
@@ -23,13 +26,17 @@ def make_request(query):
             snip = item["snippet"] if "snippet" in item else "No Snippet"
             mime = item["mime"] if "mime" in item else None
             ff = item["fileFormat"] if "fileFormat" in item else "No File Format"
-            DigiJsonBuilder.create_search_result(title, link, dlink, snip, mime, ff)
+            rjson = DigiJsonBuilder.create_search_result(title, link, dlink, snip, mime, ff)
+            resultsarr.append(rjson)
 
-
-
-    return resultsjson
+    return resultsarr
 
 
 def submit_queries(queries):
     resultsArr = []
+
+    for query in queries:
+        r = make_request(query)
+        results = DigiJsonBuilder.create_results(query, False, len(r), r)
+        resultsArr.append(results)
     return resultsArr
