e0fb31c2684dbee76daa72741c1eacb25bcaa3f3
israel <ilb8@nau.edu>
2021-04-19 00:14:23 -0700

    trying to implement an ExpandableListView for the search results stuff. Will work on this stuff some more tomorrow. Maybe ill have Kristine do it.

--- com/DigiLearn/DigitalBackpack/App/Mobile App/DigiPack/app/src/main/java/com/example/digipack/searchHistoryActivity.kt
+++ com/DigiLearn/DigitalBackpack/App/Mobile App/DigiPack/app/src/main/java/com/example/digipack/searchHistoryActivity.kt
@@ -3,9 +3,16 @@ package com.example.digipack
 import DigiJson.DigiSearch
 import android.os.Bundle
 import android.text.Html
+import android.util.Log
+import android.widget.ExpandableListAdapter
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_search_history.*
+import java.io.IOException
 
 class searchHistoryActivity : AppCompatActivity(){
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val ui = intent.getBooleanExtra("ui", false)
@@ -20,17 +27,36 @@ class searchHistoryActivity : AppCompatActivity(){
         // Change title
         supportActionBar?.title = Html.fromHtml("<font color='#01345A'>Search History</font>")
 
+        //unpack expected intent variables
+        var res : DigiSearch.DigiRes? = intent.getSerializableExtra("searchlist") as DigiSearch.DigiRes?
+        populateHistory(res?.resultslist)
+
 
 
     }
 
     private fun populateHistory(searchlist: ArrayList<DigiSearch.Results>?){
         //if the search list is null, let the user know that they dont have any results to show
-        if( searchlist != null){
-
+        if( searchlist == null){
+            viewtitle.text = getString(R.string.nosearchhistory)
         }else{
-            //else populate the
+            //else populate the search history
+            try{
+                //create string array of queries
+                var queries = ArrayList<String>()
+                for( i in searchlist ){
+                    i.query?.let { queries.add(it) }
+                }
+                //create child list objects and add them to the expandable list
 
+
+
+            }catch(e: IOException) {
+                //handle errors eventually
+                Log.e(getString(R.string.app_name), "searchHistoryActivity populateHistory error: %s".format(e.toString()))
+            }
         }
     }
+
+
 }
